c46e4e3183199c1f96173202e90f143904078884
Matthew Schulman <matthewdschulman@gmail.com>
2014-04-02 12:42:47 -0400

    close to religion working, just some minor regex issues

--- src/Religion.java
+++ src/Religion.java
@@ -1,5 +1,11 @@
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 
 public class Religion {
@@ -14,11 +20,14 @@ public class Religion {
 		
 		for (String country : countryCodes) {
 			double percentage = getPercentage(country);
-			if (percentage < 50.0) {
-				lessThan50.put(countryCodeToCountry.get(country), percentage);
-			}
-			else if (percentage > 80.0) {
-				greaterThan80.put(countryCodeToCountry.get(country), percentage);
+			//percentage > 0 if a percentage of the dominant religion was found
+			if (percentage > 0.0) {
+				if (percentage < 50.0) {
+					lessThan50.put(countryCodeToCountry.get(country), percentage);
+				}
+				else if (percentage > 80.0) {
+					greaterThan80.put(countryCodeToCountry.get(country), percentage);
+				}
 			}
 		}	
 		
@@ -33,7 +42,36 @@ public class Religion {
 	}
 
 	private static double getPercentage(String country) {
-		return 90;
+		String curCountryURL = "https://www.cia.gov/library/publications/the-world-factbook/geos/countrytemplate_"
+				+ country + ".html";
+		System.out.println(country);
+		try {
+			Document countryPage = Jsoup.connect(curCountryURL).get();
+			String pageHtml = countryPage.html();
+			
+			//make the regex pattern matching easier
+			pageHtml = pageHtml.replace("(", "");
+			pageHtml = pageHtml.replace(")", "");
+		
+			String template = "Religions:</a>\\s*.*\\s*.*\\s*.*\\s*.*\\s*.*\\s*.*data\">((\\w*|\\s*)*\\s\\d*)%";
+			Pattern p = Pattern.compile(template);			
+			Matcher m = p.matcher(pageHtml);
+			
+			if (m.find()) {
+				System.out.println(m.group(1));
+				return 0;// Double.parseDouble(m.group(1));
+			}
+			else {
+				System.out.println("no religion found");
+				return -1.0;
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Should not be here");
+		return -1.0;
 	}
 
 	private static void printCountries(HashMap<String, Double> lessThan50) {
@@ -41,6 +79,6 @@ public class Religion {
 
             String value = lessThan50.get(country).toString();  
             System.out.println(country + ": " + value + "%");
-        } 
+        }
 	}
 }
