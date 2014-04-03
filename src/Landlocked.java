333db286ccac58846779a0217e5b303570e91aae
Matthew Schulman <matthewdschulman@gmail.com>
2014-04-02 23:08:28 -0400

    finish landlocked completely

--- src/Landlocked.java
+++ src/Landlocked.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -10,6 +12,7 @@ public class Landlocked {
 
 	public static void findSingleLandlocked(LinkedList<String> countryCodes,
 			HashMap<String, String> countryCodeToCountry) {
+		System.out.println("Please wait one moment...");
 		System.out.println("The landlocked countries surronded by only country are...");
 		
 			try {
@@ -25,7 +28,6 @@ public class Landlocked {
 						if (isLandlocked) {
 							//check if the country only has one border country
 							boolean onlyOneBorderCountry = checkNeighbors(pageHtml);
-							
 							//if is landlocked and only has one border country, print country
 							if (onlyOneBorderCountry) {
 								System.out.println(countryCodeToCountry.get(country));
@@ -42,7 +44,22 @@ public class Landlocked {
 	}
 
 	private static boolean checkNeighbors(String pageHtml) {
-		return true;
+		String template = "border\\scountries:\\s*.*>(\\w*\\s.*\\skm)";
+		Pattern p = Pattern.compile(template);
+		Matcher m = p.matcher(pageHtml);
+		
+		if (m.find()) {
+			String borderCountries = m.group(1);			
+			//check if there is more than 1 border country
+			int initialWordSize = borderCountries.length();
+			borderCountries = borderCountries.replace("km", "");
+			int postRemovalWordSize = borderCountries.length();
+			if ((initialWordSize - postRemovalWordSize) > 2) {
+				return false;
+			}
+			return true;
+		}			
+		return false;
 	}
 
 	private static boolean checkIfLandlocked(String pageHtml) {
